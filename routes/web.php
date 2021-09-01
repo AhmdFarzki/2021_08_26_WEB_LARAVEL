6e4c26e5051772ab67c4b297291d38be29e21af3
AhmdFarzki <ahmfariz24@gmail.com>
2021-09-01 21:25:58 +0700

    template admin & login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::prefix('admin')
+    ->group(function() {
+        Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index']);
+    });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
